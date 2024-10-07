6310e09047b386baafb201e33f9c116799d0e28e
Reshinth23 <Reshinth67@gmail.com>
2024-10-07 14:38:56 +0530

    Delete 3js directory

--- 3js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const backToBlogLink = document.querySelector('.back-to-blog');
-    if (backToBlogLink) {
-        backToBlogLink.addEventListener('click', function (e) {
-            e.preventDefault();
-            window.location.href = '1-index.html';
-        });
-    }
-});
